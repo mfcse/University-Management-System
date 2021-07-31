59822e12c30cc6dc2351519baef695c25cb8796c
Maruf <maruf.cu.cse09@gmail.com>
2021-07-31 11:50:49 +0600

    assign course logic fixed for unassign courses in db

--- app/Http/Controllers/CourseAssignController.php
+++ app/Http/Controllers/CourseAssignController.php
@@ -65,48 +65,60 @@ class CourseAssignController extends Controller
             'department_id' => 'required',
             'teacher_id' => 'required',
             'remaining_credit' => 'required|numeric',
-            'course_code' => 'required|unique:course_assigns,course_code',
+            'course_code' => 'required',
         ];
-        $messages = [
-            'course_code.unique' => 'Course has already been asigned',
-        ];
+        // $messages = [
+        //     'course_code.unique' => 'Course has already been asigned',
+        // ];
 
-        $validator = Validator::make($request->all(), $rules, $messages);
+        $validator = Validator::make($request->all(), $rules);
         if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput();
         }
+        //check if assign==0 & already exhists
+        $course_code = $request->course_code;
+        $old_course = CourseAssign::select('assigned', 'course_code')->where('course_code', $course_code)->first();
+        dd($old_course);
+
         //check if course credit exceeds
 
         if ($request->credit_to_be_taken - $request->remaining_credit < 0 || $request->credit_to_be_taken - $request->remaining_credit < $request->credit) {
             $this->setErrorMessage('Teacher Credit Quota exceeds');
             return redirect()->back();
-
-            // echo '<script>
-            // var answer = window.confirm("Are you Sure to assign more credits than limit?");
-            //     if (answer) {
-            //         console.log("ok");
-            //     }
-            //     else {
-
-            //     }
-            // </script>';
-            // <script>alert('do you want to continue?')</script>
         }
-
 
         //insert
 
         try {
             //some logics to be implemented
-            CourseAssign::create([
-                'department_id' => $request->department_id,
-                'teacher_id' => $request->teacher_id,
-                'remaining_credit' => $request->remaining_credit - $request->course_credit,
-                //'course_id' => $request->course_id,
-                'course_code' => $request->course_code,
-            ]);
-            $this->setSuccessMessage('Course Assigned Successfully');
-            return redirect()->back();
+            //update old course
+            if ($old_course) {
+                if ($old_course->assigned === 1) {
+                    $this->setErrorMessage('Course has already assigned');
+                    return redirect()->back();
+                } else {
+                    CourseAssign::where('assigned', 0)->where('course_code', $request->course_code)->update([
+                        'department_id' => $request->department_id,
+                        'teacher_id' => $request->teacher_id,
+                        'remaining_credit' => $request->remaining_credit - $request->course_credit,
+                        //'course_id' => $request->course_id,
+                        'course_code' => $request->course_code,
+                        'assigned' => 1
+                    ]);
+                }
+            } else {
+                //new course
+                CourseAssign::create([
+                    'department_id' => $request->department_id,
+                    'teacher_id' => $request->teacher_id,
+                    'remaining_credit' => $request->remaining_credit - $request->course_credit,
+                    //'course_id' => $request->course_id,
+                    'course_code' => $request->course_code,
+                ]);
+
+                $this->setSuccessMessage('Course Assigned Successfully');
+                return redirect()->back();
+            }
         } catch (Exception $e) {
             $this->setErrorMessage($e->getMessage());
         }
@@ -114,9 +126,11 @@ class CourseAssignController extends Controller
     }
     public function unassignCourses()
     {
+
         //dd(CourseAssign::where('assigned', 1)->get());
         $course = CourseAssign::where('assigned', 1)->update([
-            'assigned' => 0
+            'assigned' => 0,
+            // 'remaining_credit'=>
         ]);
         $this->setSuccessMessage('All Courses Unassigned Successfully');
         return view('course.unassign');
